79e2067ffb370a24ba38f34386f5edd4c5725d54
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-24 21:24:05 +0800

    finished building tree

--- lib/binary_tree/tree.rb
+++ lib/binary_tree/tree.rb
@@ -1,7 +1,105 @@
 module BinaryTree
 	class Tree
-		def build_tree(arr)
-			return Node.new("123")
+		attr_accessor :tree_ref
+		def initialize
+			@tree_ref = nil
 		end
+
+		def build_tree(array)
+			array.each do |x|
+				insert(x)
+			end
+		end
+
+		def insert(data)
+			if tree_ref == nil
+				@tree_ref = Node.new(data)
+			else
+				tmp = tree_ref
+				found_place = false
+				while !found_place
+					if data <= tmp.data
+						if tmp.left_child == nil
+							tmp.left_child = Node.new(data)
+							tmp.left_child.parent = tmp
+							found_place = true
+							break
+						else
+							tmp = tmp.left_child
+						end
+					else
+						#data >= tmp.data
+						if tmp.right_child == nil
+							tmp.right_child = Node.new(data)
+							tmp.right_child.parent = tmp
+							found_place = true
+							break
+						else
+							tmp = tmp.right_child
+						end
+
+					end
+				end
+			end
+		end
+
+		def breadth_search(data)
+			return nil if tree_ref == nil
+			array_queue = []
+			tmp = tree_ref
+			array_queue.push(tmp)
+
+			while !array_queue.empty?
+				puts
+				puts array_queue.last.data
+				return array_queue.last if array_queue.last.data == data
+				array_queue.unshift(array_queue.last.left_child) if array_queue.last.left_child != nil
+				array_queue.unshift(array_queue.last.right_child) if array_queue.last.right_child != nil
+				array_queue.pop
+			end
+			return "Not Found"
+		end
+
+		def depth_search(search_data)
+			stack = [tree_ref]
+			visited = [tree_ref]
+
+			while !stack.empty?
+				current = stack.last
+
+				puts current.data
+				left,right = current.left_child, current.right_child
+
+				if current.data == search_data
+					return current
+				elsif !left.nil? && !visited.include?(left)
+					if left.data == search_data
+						return left
+					else
+						stack << left
+						visited << left
+					end
+				elsif !right.nil? && !visited.include?(right)
+					if right.data == search_data
+						return right
+					else
+						stack << right
+						visited << right
+					end
+				else
+					stack.pop
+				end
+			end
+		end
+
+		def dfs_rec(data,current_node=tree_ref)
+			return if current_node == nil
+			puts current_node.data
+			return current_node if current_node.data == data
+			a = dfs_rec(data,current_node.left_child)
+			b = dfs_rec(data,current_node.right_child)
+			a || b
+		end
+
 	end
 end
--- spec/spec_tree.rb
+++ spec/spec_tree.rb
@@ -2,6 +2,43 @@ require 'spec_helper'
 
 module BinaryTree
 	context "#build_tree(arr)" do
-		
+		it "test" do
+			t = Tree.new
+			t.build_tree(["a","b","c"])
+		end
 	end 
+
+	context "#breadth_search(data)" do
+		it "returns correct node" do
+			t = Tree.new
+			a = [5,2,3,4,1,6,7,8,9,10]
+			t.build_tree(a)
+			expect(t.breadth_search(10).data).to eq 10
+		end
+	end
+
+	context "#depth_search(search_data)" do
+		it "returns correct node" do
+			t = Tree.new
+			a = [5,2,3,4,1,6,7,8,9,10]
+			t.build_tree(a)
+			expect(t.depth_search(9).data).to eq 9
+		end
+	end
+
+	context "#dfs_rec(data,current_node)" do
+		it "returns correct node" do
+			t = Tree.new
+			a = [5,2,3,4,1,6,7,8,9,10]
+			t.build_tree(a)
+			expect(t.dfs_rec(5).data).to eq 5
+		end
+
+		it "returns correct node #2" do
+			t= Tree.new
+			a = [5,2,3,4,1,6,7,8,9,10]
+			t.build_tree(a)
+			expect(t.dfs_rec(4).data).to eq 4
+		end
+	end
 end
